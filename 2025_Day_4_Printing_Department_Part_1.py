ab1457bbc30743fd67b3171e5ef52fee22657748
QuixoticDango <lsphdgrad2019@gmail.com>
2025-12-04 21:49:56 -0500

    Create 2025_Day_4_Printing_Department_Part_1.py

---
+++ 2025_Day_4_Printing_Department_Part_1.py
@@ -0,0 +1,29 @@
+from itertools import product
+
+file = "C:\\Users\\lyndo\\Documents\\Coding and Programming Folder\\2025 Day 4 Advent of Code Input.txt"
+
+with open(file, 'rt') as f:
+    grid = [row.strip() for row in f.readlines()]
+
+dir = list(product([i for i in range(-1, 2, 1)], repeat=2))
+dir = [tup for tup in dir if tup != (0,0)]
+# print(dir)
+
+accessible_paper_rolls = 0
+for i, row in enumerate(grid):
+    for j, column in enumerate(row):
+        if grid[i][j] != '@':
+            continue
+        checksum = 0
+        for k in range(8):
+            try:
+                if i + dir[k][0] < 0 or j + dir[k][1] < 0:
+                    raise NegativeIndex
+                if grid[i + dir[k][0]][j + dir[k][1]] == '@':
+                    checksum += 1
+            except:
+                pass
+        if checksum < 4:
+            accessible_paper_rolls += 1
+
+print(f"{accessible_paper_rolls=}")
